311f2fdcc50a1ab5094740aa20398f92894fcc62
fabiomdf <fabiomdf@gmail.com>
2025-07-18 16:29:27 +0200

    Removed blank lines

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,13 +14,11 @@ const childBox12 = ref()
   <DraggableBox ref="parentBox" :initialPosition="{ x: 200, y: 100 }">Parent</DraggableBox>
   <DraggableBox ref="childBox1" :initialPosition="{ x: 100, y: 250 }">Child 1</DraggableBox>
   <DraggableBox ref="childBox2" :initialPosition="{ x: 300, y: 250 }">Child 2</DraggableBox>
-
   <DraggableBox ref="childBox11" :initialPosition="{ x: 50, y: 400 }">Child 1.1</DraggableBox>
   <DraggableBox ref="childBox12" :initialPosition="{ x: 250, y: 400 }">Child 1.2</DraggableBox>
 
   <ConnectionLine :start="parentBox" :end="childBox1" />
   <ConnectionLine :start="parentBox" :end="childBox2" />
-
   <ConnectionLine :start="childBox1" :end="childBox11" />
   <ConnectionLine :start="childBox1" :end="childBox12" />
 </template>
